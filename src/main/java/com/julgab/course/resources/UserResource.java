987cccd4dfd037b7efe27b1196e6cf7add596cf0
Guilherme Souza <guilhermejrs@gmail.com>
2021-03-24 17:46:11 -0300

    User insert

--- src/main/java/com/julgab/course/resources/UserResource.java
+++ src/main/java/com/julgab/course/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.julgab.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.julgab.course.entities.User;
 import com.julgab.course.services.UserService;
@@ -29,4 +33,20 @@ public class UserResource {
 		return ResponseEntity.ok().body(service.findById(id));
 	}
 
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		
+		obj = service.insert(obj);
+		/*
+		 * Está ok, retorna return code 200, mas o desejado é que retorne 201:
+		 *
+		return ResponseEntity.ok().body(obj);
+		*/
+		/*
+		 * Está ok, retorna return code 201, que é o desejado:
+		 */
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+
+	}
 }
--- src/main/java/com/julgab/course/services/UserService.java
+++ src/main/java/com/julgab/course/services/UserService.java
@@ -23,4 +23,8 @@ public class UserService {
 //		return obj.get();
 		return (repository.findById(id).get());
 	}
+	
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
 }
